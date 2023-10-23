f80013df321a42fa740d2ac234004115e6df58ac
stas312s <qwerttwerq2000@gmail.com>
2023-10-23 19:46:12 +0300

    Fixed menu

--- Assets/Scripts/MenuManager/MenuManager.cs
+++ Assets/Scripts/MenuManager/MenuManager.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField]private Button _playButton;
+
+    private void Start()
+    {
+        _playButton.onClick.AddListener(Play);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("GameScene");
