85ecfab55886121c59dea5207670c8a22ecc9a2c
MazankaQQW <136856664+MazankaQQW@users.noreply.github.com>
2023-11-20 02:35:34 +0300

    Update Inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,12 +1,22 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    public static Inventory instance;
     public List<Item> items = new List<Item>();
     public Transform slotsParent; // Родительский объект для слотов в интерфейсе
     public GameObject slotPrefab; // Префаб слота в интерфейсе
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     private void Start()
     {
@@ -34,7 +44,7 @@ public class Inventory : MonoBehaviour
         Image slotImage = slot.GetComponent<Image>();
         if (slotImage != null)
         {
-            slotImage.sprite = item.Icon; // Предполагается, что у Item есть свойство icon
+            slotImage.sprite = item.icon; // Предполагается, что у Item есть свойство icon
         }
 
         // Дополните этот метод, чтобы обрабатывать другие аспекты предмета в слоте, например, текстовые метки и дополнительные данные
--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public Sprite Icon;
+    public string itemName; // Название предмета
+    public Sprite icon; // Изображение предмета (для отображения в инвентаре)
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Inventory.instance.AddItem(this);
+            gameObject.SetActive(false); // Скрываем объект предмета после подбора
+           
+        }
+    }
 }
