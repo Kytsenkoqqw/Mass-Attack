18cc768e4d2ea8583b1a32ba82eb4d95491bb783
MazankaQQW <136856664+MazankaQQW@users.noreply.github.com>
2023-11-26 17:08:03 +0300

    Add AxeSpawn

--- Assets/Scripts/ItemSkills/SkillsManager.cs
+++ Assets/Scripts/ItemSkills/SkillsManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ItemSkills
@@ -6,11 +8,19 @@ namespace ItemSkills
     public class SkillsManager : MonoBehaviour
     {
         public static SkillsManager instance;
-        public Transform heroTransform; // Перетащите сюда Transform героя в инспекторе Unity
-        public GameObject summonPrefab; // Префаб саммона
+        public Transform heroTransform; 
+        public GameObject summonPrefab; 
         public int numberOfSummons = 2;
         public float SpawnDistance = 2f;
-
+        public GameObject axePrefab;
+        public float rotationSpeed = 180f; // Скорость вращения топоров
+        public float axeRadius = 5f; // Радиус, в котором появляются топоры
+        public float axeDuration = 5f;
+        public float BaseAngle;
+        
+        private float _angle = 0.0f;
+        private float _currentAngle = 0.0f;    
+        
         public void Awake()
         {
             if (instance == null)
@@ -35,6 +45,11 @@ namespace ItemSkills
             }
         }
 
+        private void Update()
+        {
+            RotateAxe();
+        }
+
         public void GetSkill(ItemSpell spell)
         {
             switch (spell)
@@ -42,6 +57,8 @@ namespace ItemSkills
                 case ItemSpell.Bow:
                     break;
                 case ItemSpell.Axe:
+                    SpawnAxes();
+                    Debug.Log("Work!!!");
                     break;
                 case ItemSpell.Ring:
                     break;
@@ -67,12 +84,41 @@ namespace ItemSkills
 
                 // Создание саммона в вычисленной позиции
                 GameObject summon = Instantiate(summonPrefab, summonPosition, Quaternion.identity);
-
-                // Дополнительные настройки или действия с саммоном, если необходимо
-                // Например, можно добавить компоненты, управляющие поведением саммона и т.д.
             }
         }
+        
+        private void SpawnAxes()
+        {
+            // Цикл для создания топоров
+            for (int i = 0; i < 360; i += 45) // Используем шаг 45 градусов для создания восьми топоров вокруг героя
+            {
+                // Вычисление позиции для топора вокруг героя
+                float angle = i * Mathf.Deg2Rad;
+                Vector3 summonPosition = heroTransform.position + new Vector3(Mathf.Cos(angle) * axeRadius, 0, Mathf.Sin(angle) * axeRadius);
+
+                // Создание топора в вычисленной позиции с начальной ориентацией
+                GameObject axe = Instantiate(axePrefab, summonPosition, Quaternion.identity);
+            }
+        }
+        
+        private void RotateAxe()
+        {
+            _angle += rotationSpeed * Time.deltaTime;
+
+            // Вычисляем новую позицию сферы вокруг героя.
+            float x = heroTransform.position.x + Mathf.Cos(_angle + BaseAngle) * axeRadius;
+            float z = heroTransform.position.z + Mathf.Sin(_angle + BaseAngle) * axeRadius;
+
+            // Устанавливаем новую позицию сферы.
+            transform.position = new Vector3(x, transform.position.y, z);
+            
+        }
+
+        private IEnumerator DestroyAxe()
+        {
+            RotateAxe();
+            yield return new WaitForSeconds(2f);
+            Destroy(gameObject);
+        }
     }
-    
-    
 }
